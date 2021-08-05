42fa0e87b4f4bfd744c500cfe831f737e337878a
Asfandyar Jamali <asfand.edtech@gmail.com>
2021-08-05 23:53:27 +0500

    remove database credentials from next config file

--- next.config.js
+++ next.config.js
@@ -1,25 +1,3 @@
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
-
-module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      reactStrictMode: true,
-      env: {
-        mongodb_username: "asfand",
-        mongodb_password: "78asfand38",
-        mongodb_clustername: "cluster0",
-        mongodb_database: "my-blog-dev",
-      },
-    };
-  }
-
-  return {
-    reactStrictMode: true,
-    env: {
-      mongodb_username: "asfand",
-      mongodb_password: "78asfand38",
-      mongodb_clustername: "cluster0",
-      mongodb_database: "my-blog",
-    },
-  };
+module.exports = {
+  reactStrictMode: true,
 };
